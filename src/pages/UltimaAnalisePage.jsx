ed4725e3cd72e4dba5a88fb3666e68003e17ca65
Sanmyr <145044521+SanmyrB@users.noreply.github.com>
2026-02-05 10:30:34 -0400

    Update UltimaAnalisePage.jsx

--- src/pages/UltimaAnalisePage.jsx
+++ src/pages/UltimaAnalisePage.jsx
@@ -255,7 +255,7 @@ const UltimaAnalisePage = () => {
   const corClassificacao = getCorClassificacao(analise.classif);
 
   return (
-    <div className="min-h-screen bg-gray-100 p-6">
+    <div className=`min-h-screen ${corClassificacao} p-6`>
       <div className="max-w-4xl mx-auto">
         <div className="flex justify-between items-center mb-6">
           <div>
